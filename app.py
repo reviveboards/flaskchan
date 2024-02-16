cf32e062b261be5f4abcdc2611825f9e05639f5c
Rehdzi <5crkurwa@gmail.com>
2024-02-16 12:07:34 +0300

    creating boards and categories, latest posts

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
-import datetime
+
+from flask_wtf import CSRFProtect
+
 from util.config import load_config, random_motd
 from util.logging import *
 from util.db import *
 from util.forms import *
 
+
 app = Flask(__name__)
 
+app.config.update(
+    TESTING=True,
+    SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf',
+    WTF_CSRF_SECRET_KEY='v rossiyskom monitore obnaruzhili kitayskiy chip'
+)
+
 date = datetime.date.today()
+
+csrf = CSRFProtect()
 
 config = load_config('config.json')
 
 version = "0.1a"
+
 
 if config['logging']:
     current_log = start_logging(config['logs_path']).name
@@ -19,6 +31,7 @@ if config['logging']:
 else:
     log_config(config, '')
 
+csrf.init_app(app)
 
 # ############ #
 # Some getters #
@@ -47,6 +60,28 @@ def get_boards():
     conn.close()
 
     return all_boards
+
+
+def get_this_board(t):
+    connection = get_db_conn()
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM board WHERE tag = '{t}';")
+    this_board = cursor.fetchone()
+    cursor.close()
+    connection.close()
+
+    return this_board
+
+
+def get_latest_posts():
+    conn = get_db_conn()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM post JOIN board ON post.board_id = board.id WHERE board.nsfw = false ORDER BY timestamp DESC LIMIT 8')
+    latest_posts = cur.fetchall()
+    cur.close()
+    conn.close()
+
+    return latest_posts
 
 
 def get_board_posts(board_id):
@@ -119,12 +154,15 @@ def boards():
 
     list_boards = get_boards()
 
+    p_latest = get_latest_posts()
+
     title = config['title']
     motd = random_motd(config)
 
     return render_template('boards.html',
                            categories=categories,
                            list_boards=list_boards,
+                           p_latest=p_latest,
                            date=date,
                            title=title,
                            motd=motd,
@@ -140,27 +178,26 @@ def boards():
 
 @app.route('/<string:board_tag>', methods=['GET', 'POST'])
 def get_board(board_tag):
-    connection = get_db_conn()
-    cursor = connection.cursor()
-    cursor.execute(f"SELECT * FROM board WHERE tag = '{board_tag}';")
-    this_board = cursor.fetchone()
-    cursor.close()
-    connection.close()
 
-    if request.method == 'POST':
-        title = request.form['postTitle']
+    tb = get_this_board(board_tag)
+
+    n_p_form = NewPostForm()
+
+    if n_p_form.validate_on_submit():
+        p_title = n_p_form.post_title.data
         parent_post = 0
-        parent_board = this_board[0]
-        post_text = request.form['postText']
+        parent_board = tb[0]
+        p_text = n_p_form.post_text.data
         images = [1, 1, 1]
 
-        create_post(title, parent_post, parent_board, post_text, images, this_board[1])
+        create_post(p_title, parent_post, parent_board, p_text, images, tb[1])
 
-    posts = get_board_posts(this_board[0])
+    posts = get_board_posts(tb[0])
 
     return render_template('board.html',
                            date=date,
-                           this_board=this_board,
+                           this_board=tb,
+                           n_p_form=n_p_form,
                            posts=posts,
                            version=version)
 
@@ -169,12 +206,29 @@ def get_board(board_tag):
 def admin():
     admin_categories = get_categories()
 
-    
+    n_b_form = NewBoardForm()
+    n_c_form = NewCategoryForm()
+
+    n_b_form.board_category.choices = [(cat[0], cat[1]) for cat in admin_categories]
+
+    if n_b_form.validate_on_submit():
+        b_name = n_b_form.board_name.data
+        b_desc = n_b_form.board_description.data
+        b_tag = n_b_form.board_tag.data
+        b_cat_id = n_b_form.board_category.data
+
+        create_board(b_tag, b_cat_id, b_name, b_desc)
+    elif n_c_form.validate_on_submit():
+        c_name = n_c_form.category_name.data
+
+        create_category(c_name)
 
     return render_template('admin.html',
                            admin_categories=admin_categories,
                            date=date,
-                           version=version)
+                           version=version,
+                           n_b_form=n_b_form,
+                           n_c_form=n_c_form)
 
 
 if __name__ == '__main__':
--- util/forms.py
+++ util/forms.py
@@ -1,10 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, BooleanField
+from wtforms import StringField, SubmitField, BooleanField, SelectField
 from wtforms.validators import InputRequired, Length
 
 
 class NewBoardForm(FlaskForm):
-    board_name = StringField("Board name", validators=[InputRequired(), Length(min=2, max=50)])
-    board_description = StringField("Board description", validators=[Length(min=0, max=150)])
-    board_tag = StringField("Board tag", validators=[Length(min=1, max=5)])
+    board_name = StringField("Board name", render_kw={"placeholder": "Board name"}, validators=[InputRequired(), Length(min=2, max=50)])
+    board_description = StringField("Board description", render_kw={"placeholder": "Board description"}, validators=[Length(min=0, max=150)])
+    board_tag = StringField("Board tag", render_kw={"placeholder": "Board tag"}, validators=[Length(min=1, max=5)])
+    board_category = SelectField("Board category", coerce=int)
+
     create_board = SubmitField("Create new board")
+
+
+class NewCategoryForm(FlaskForm):
+    category_name = StringField("Category name", validators=[InputRequired(), Length(min=1, max=50)])
+
+    create_category = SubmitField("Create new category")
+
+
+class NewPostForm(FlaskForm):
+    post_title = StringField("Title", render_kw={"placeholder": "Title", "id": "pH"}, validators=[Length(min=0, max=150)])
+    post_text = StringField("Text", render_kw={"placeholder": "Text", "id": "pT"}, validators=[Length(min=0, max=450)])
+    create_post = SubmitField("+ Post")
